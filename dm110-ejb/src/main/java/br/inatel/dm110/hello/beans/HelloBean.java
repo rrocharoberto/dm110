bebff91a0b4deb5d91170acee8c6481a10dc2a71
Roberto Rocha <rrocha.roberto@gmail.com>
2019-11-09 16:35:23 -0200

    Prática aula 3.

--- dm110-ejb/src/main/java/br/inatel/dm110/hello/beans/HelloBean.java
+++ dm110-ejb/src/main/java/br/inatel/dm110/hello/beans/HelloBean.java
@@ -1,5 +1,6 @@
 package br.inatel.dm110.hello.beans;
 
+import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
@@ -11,12 +12,18 @@ import br.inatel.dm110.hello.interfaces.HelloRemote;
 @Remote(HelloRemote.class)
 @Local(HelloLocal.class)
 
-public class HelloBean implements HelloLocal, HelloRemote{
+public class HelloBean implements HelloLocal, HelloRemote {
+
+	@EJB
+	private HelloMessageSender msgSender;
 
 	@Override
 	public String sayHello(String name) {
 		System.out.println("Chamou o Hello Bean: " + name);
+
+		msgSender.sendTextMessage(name);
+		System.out.println("HelloBean: Mensagem enviada para a queue.");
+
 		return "Hello " + name + " !!!";
 	}
 }
-
