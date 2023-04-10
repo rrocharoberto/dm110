132d312c6ab588d5f02dfd9bcd0c44b3cfe4eec4
Roberto Rocha <rrocha.roberto@gmail.com>
2023-04-09 22:17:03 -0300

    Prepare for next class

--- dm110-ejb/src/main/java/br/inatel/dm110/beans/example/HelloQueueSender.java
+++ dm110-ejb/src/main/java/br/inatel/dm110/beans/example/HelloQueueSender.java
@@ -1,38 +1,13 @@
 package br.inatel.dm110.beans.example;
 
-import java.util.logging.Logger;
-
-import javax.annotation.Resource;
 import javax.ejb.Stateless;
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
 
 @Stateless
 public class HelloQueueSender {
 
-	@Resource(lookup = "java:/ConnectionFactory")
-	private ConnectionFactory connectionFactory;
+	//TODO: implement it
 	
-	@Resource(lookup = "java:/jms/queue/dm110queue")
-	private Queue queue;
-	
-	private static Logger log = Logger.getLogger(HelloQueueSender.class.getName());
-
 	public void sendTextMessage(String text) {
-		try {
-			Connection conn = connectionFactory.createConnection();
-			Session session = conn.createSession();
-			MessageProducer msgProducer = session.createProducer(queue);
-			TextMessage txtMsg = session.createTextMessage(text);
-			msgProducer.send(txtMsg);
-			log.info("Enviou a mensagem: " + txtMsg);
-		} catch (JMSException e) {
-			throw new RuntimeException(e);
-		}
+		//TODO: implement it
 	}
 }
--- dm110-mdb/src/main/java/br/inatel/dm110/beans/example/HelloQueueMDB.java
+++ dm110-mdb/src/main/java/br/inatel/dm110/beans/example/HelloQueueMDB.java
@@ -2,34 +2,18 @@ package br.inatel.dm110.beans.example;
 
 import java.util.logging.Logger;
 
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.MessageDriven;
-import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
-import javax.jms.TextMessage;
 
-@MessageDriven(activationConfig = {
-		@ActivationConfigProperty(propertyName = "destinationType", 
-								  propertyValue = "javax.jms.Queue"),
-		@ActivationConfigProperty(propertyName = "destination", 
-								  propertyValue = "java:/jms/queue/dm110queue") })
+//TODO: implement it
 
 public class HelloQueueMDB implements MessageListener {
-
+	
 	private static Logger log = Logger.getLogger(HelloQueueMDB.class.getName());
 
 	@Override
 	public void onMessage(Message message) {
-		// processamento da mensagem
-		try {
-			if (message instanceof TextMessage) {
-				TextMessage txtMessage = (TextMessage) message;
-				String text = txtMessage.getText();
-				log.info("Mensagem recebida da fila: " + text);
-			}
-		} catch (JMSException e) {
-			e.printStackTrace();
-		}
+		//processamento da mensagem
+		//TODO: implement it
 	}
 }
