5219039c1d354a796a85d73fa8956b53054f68eb
Roberto Rocha <rrocha.roberto@gmail.com>
2023-04-09 21:11:45 -0300

    Prepare for next class

--- dm110-app-client/src/main/java/br/inatel/dm110/client/hello/HelloClient.java
+++ dm110-app-client/src/main/java/br/inatel/dm110/client/hello/HelloClient.java
@@ -13,9 +13,9 @@ import br.inatel.dm110.api.example.MessageTO;
 
 public class HelloClient {
 
-	private static final String REST_URI_GET = "http://localhost:8080/dm110-web/api/hello/message";
-	private static final String REST_URI_GET_ALL = "http://localhost:8080/dm110-web/api/hello/messages";
-	private static final String REST_URI_POST = "http://localhost:8080/dm110-web/api/hello/message";
+	private static final String REST_BASE_URI = "http://localhost:8080/dm110-web/api/hello";
+	private static final String REST_URI_GET = REST_BASE_URI + "/message";
+	private static final String REST_URI_POST = REST_URI_GET;
 
 	private static Client client = ClientBuilder.newClient();
 
@@ -23,16 +23,12 @@ public class HelloClient {
 		System.out.println("Message service:");
 		
 		MessageTO msg = new MessageTO();
-		msg.setFirstName("Jonas");
-		msg.setLastName("Oliveira");
-		
+		msg.setFirstName("Roberto");
+		msg.setLastName("Rocha");
 		System.out.println("Result from POST: " + createMessage(msg));
 
-		System.out.println("Result from GET 0: " + getMessage(0));
 		System.out.println("Result from GET 1: " + getMessage(1));
 		System.out.println("Result from GET 2: " + getMessage(2));
-		
-		System.out.println("Result of all messages: " + getAllMessages());
 	}
 
 	public static MessageTO getMessage(Integer id) {
@@ -49,13 +45,13 @@ public class HelloClient {
 	      .request(MediaType.APPLICATION_JSON)
 	      .post(Entity.entity(msg, MediaType.APPLICATION_JSON));
 	}
-	
-	public static List<MessageTO> getAllMessages() {
-		List<MessageTO> list = client
-                .target(REST_URI_GET_ALL)
-                .request(MediaType.APPLICATION_JSON)
-                .get(Response.class)
-                .readEntity(new GenericType<List<MessageTO>>() {});
-		return list;
-	}
+
+	//public static List<MessageTO> getAllMessages() {
+	//	List<MessageTO> list = client
+    //            .target(REST_URI_GET_ALL)
+    //            .request(MediaType.APPLICATION_JSON)
+    //            .get(Response.class)
+    //            .readEntity(new GenericType<List<MessageTO>>() {});
+	//	return list;
+	//}
 }
