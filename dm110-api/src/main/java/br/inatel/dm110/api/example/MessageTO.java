cff9bcd18924a6ef413c8b79e5aa6699e5b1ef33
Roberto Rocha <rrocha.roberto@gmail.com>
2024-04-30 08:45:39 -0300

    Add Lombok support

--- dm110-api/src/main/java/br/inatel/dm110/api/example/MessageTO.java
+++ dm110-api/src/main/java/br/inatel/dm110/api/example/MessageTO.java
@@ -2,6 +2,13 @@ package br.inatel.dm110.api.example;
 
 import java.io.Serializable;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class MessageTO implements Serializable {
 
 	private static final long serialVersionUID = -5106558229027863908L;
@@ -9,34 +16,5 @@ public class MessageTO implements Serializable {
 	private String firstName;
 	private String lastName;
 	private String message;
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	@Override
-	public String toString() {
-		return "MessageTO [firstName=" + firstName + ", lastName=" + lastName + "]";
-	}
 	
 }
